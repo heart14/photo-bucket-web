e8cfcaa8a9c7f93562b6eb08e58658b213d808db
heart14 <lwf14@qq.com>
2022-08-30 11:26:49 +0800

    init project

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="head">
+      <div class="logo"><img alt="Vue logo" src="@/assets/logo.png" /></div>
+      <div class="title"><h1>Photo Bucket</h1></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+.head{
+    background-color: #f6f5ec;
+    height: 50px;
+    line-height: 50px;
+}
+.logo{
+    position: fixed;
+    float: left;
+}
+img{
+    width: 40px;
+    height: 40px;
+    padding: 5px;
+    padding-left: 25px;
+}
+</style>
---
+++ src/view/PhotoUploadPage/PhotoUpload.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Navbar />
+  </div>
+</template>
+
+<script>
+import Navbar from "../../components/Navbar.vue";
+
+export default {
+  name: "PhotoUpload",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style scoped>
+.logo {
+  float: left;
+  margin: 0;
+}
+.title {
+  margin: 0;
+}
+</style>
